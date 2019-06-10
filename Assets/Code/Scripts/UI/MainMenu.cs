b2f5d0cd7cb506846130864d9c0e1db32699fd10
Nye <nyegoodall@gmail.com>
2019-06-10 06:20:11 +0100

    Added text representation of volume slider

--- Assets/Code/Scripts/UI/MainMenu.cs
+++ Assets/Code/Scripts/UI/MainMenu.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+
+	public static string displayVolume = "100%";
+
     public void PlayGame ()
     {
     	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,5 +18,18 @@ public class MainMenu : MonoBehaviour
     {
     	Debug.Log("QUIT");
     	Application.Quit();
+    }
+
+    public AudioMixer audioMixer;
+
+    public void SetVolume (float volume)
+    {
+    	audioMixer.SetFloat("volume", volume);
+    }
+
+    public void GetVolume (float volume)
+    {
+    	// Debug.Log("Volume is: " + displayVolume);
+    	displayVolume = Mathf.FloorToInt((volume + 80) * 5/4) + "%";
     }
 }
---
+++ Assets/Code/Scripts/UI/VolumeText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VolumeText : MonoBehaviour
+{
+
+	private TextMeshProUGUI volumeText;
+
+	void Start()
+	{
+		volumeText = GetComponent<TextMeshProUGUI> ();
+	}
+
+    void Update()
+    {
+    	volumeText.text = MainMenu.displayVolume;
+    	// Debug.Log("The current volume is: " + MainMenu.displayVolume);
+    }
+}

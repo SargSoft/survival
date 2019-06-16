da9cd55f213e066bc667338145c450b6e6423305
Jack-G-T <47399716+Jack-G-T@users.noreply.github.com>
2019-06-16 17:40:31 +0100

    Added StickObjectToBelow Function and changed public var to serialized private
    Also further cleaned code, optimized functions etc.

--- Assets/Code/Scripts/Environment/PlantLife.cs
+++ Assets/Code/Scripts/Environment/PlantLife.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class PlantLife : MonoBehaviour {
 
-    public GameObject floor; // Refrence to the ocean floor prefab
-    public GameObject plant; // Refrence to plant object to spawn in
-    public GameObject midPlant; // Second plant
-    public int numOfPlants; // number of plants to spawn
+    [SerializeField] private GameObject floor; // Refrence to the ocean floor prefab
+    [SerializeField] private GameObject plant; // Refrence to plant object to spawn in
+    [SerializeField] private GameObject midPlant; // Second plant
+    [SerializeField] private int numOfPlants; // number of plants to spawn
 
     void Start() {
         //Populate shallow depth bounding boxes with required plants
         foreach (GameObject shallowsBox in GameObject.FindGameObjectsWithTag("shallows")) {
-        	Debug.Log("one sPlant in shalllows");
             CreatePlants(numOfPlants, shallowsBox, plant, GameObject.Find("Plants"));
         }
         //Populate mid depth bounding boxes with required plants
@@ -20,29 +19,25 @@ public class PlantLife : MonoBehaviour {
             CreatePlants(numOfPlants, mediumBox, midPlant, GameObject.Find("MidPlants"));
         }
     }
-    // Creates a set number of plants(arg1) in and area(arg2) of type(arg3) and makes them child of(arg4).
-    void CreatePlants(int number, GameObject area, GameObject plantType, GameObject parent) {      
-        for (var i = 0; i <= number; i++) {
-            //Instantiate plant object and assign it a name while making it a child of nearest plants.
-            GameObject thisPlant = Instantiate(plantType, FindPlantPosition(area), plantType.transform.rotation);
-            thisPlant.name = plantType.name;
-            thisPlant.transform.parent = parent.transform;
-            // Use raycast to set plant to same height as area
-            if(thisPlant.transform.position.y > floor.transform.position.y) {
-                RaycastHit hit;
-                Vector3 dwn = thisPlant.transform.TransformDirection(Vector3.down);
-                if(Physics.Raycast(thisPlant.transform.position, dwn, out hit, 50)) {
-                    thisPlant.transform.position = hit.point;
-                }
-            }
-        }
-    }
     // Returns a random xz position within the area occupied by a game object and spawns at a set height (raycast used elsewhere "sticks" spawned object to "area" object)
     public Vector3 FindPlantPosition(GameObject obj) {
         Renderer r = obj.GetComponent<Renderer>();
-        float randomX = Random.Range(r.bounds.min.x, r.bounds.max.x);
-        float randomZ = Random.Range(r.bounds.min.z, r.bounds.max.z);
-        Vector3 spawnPosition = new Vector3(randomX, 50.0f, randomZ);
-        return spawnPosition;
+        return new Vector3(Random.Range(r.bounds.min.x, r.bounds.max.x), 50.0f, Random.Range(r.bounds.min.z, r.bounds.max.z));
+    }
+    // Sticks obj to an object below it
+    public void StickObjectToBelow(GameObject obj, GameObject below) {
+        RaycastHit hit;
+        Vector3 dwn = obj.transform.TransformDirection(Vector3.down);
+        if(Physics.Raycast(obj.transform.position, dwn, out hit, 50)) obj.transform.position = hit.point;
+    }
+    // Creates a set number of plants(arg1) in and area(arg2) of type(arg3) and makes them child of(arg4).
+    public void CreatePlants(int number, GameObject area, GameObject plantType, GameObject parent) {      
+        for (var i = 0; i <= number; i++) {
+            //Instantiate plant object and assign it a name while making it a child of nearest plants.
+            GameObject thisPlant = Instantiate(plantType, FindPlantPosition(area), plantType.transform.rotation);
+            StickObjectToBelow(thisPlant, floor);
+            thisPlant.name = plantType.name;
+            thisPlant.transform.parent = parent.transform;         
+        }
     }
 }

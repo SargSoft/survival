d461a0a2057dd9008d952dcc6183651a61034fc6
Jack-G-T <47399716+Jack-G-T@users.noreply.github.com>
2019-06-16 20:57:17 +0100

    Plant spawning now automatically detects height/type while instantiating

--- Assets/Code/Scripts/Environment/PlantLife.cs
+++ Assets/Code/Scripts/Environment/PlantLife.cs
@@ -5,18 +5,14 @@ using UnityEngine;
 public class PlantLife : MonoBehaviour {
 
     [SerializeField] private GameObject floor; // Refrence to the ocean floor prefab
-    [SerializeField] private GameObject plant; // Refrence to plant object to spawn in
-    [SerializeField] private GameObject midPlant; // Second plant
+    [SerializeField] private GameObject placeHolderPlant;
+    [SerializeField] private GameObject shallowPlant; // Refrence to plant object to spawn in
+    [SerializeField] private GameObject deepPlant; // Refrence to plant object to spawn in
     [SerializeField] private int numOfPlants; // number of plants to spawn
 
     void Start() {
-        //Populate shallow depth bounding boxes with required plants
-        foreach (GameObject shallowsBox in GameObject.FindGameObjectsWithTag("shallows")) {
-            CreatePlants(numOfPlants, shallowsBox, plant, GameObject.Find("Plants"));
-        }
-        //Populate mid depth bounding boxes with required plants
-        foreach (GameObject mediumBox in GameObject.FindGameObjectsWithTag("mid")) {
-            CreatePlants(numOfPlants, mediumBox, midPlant, GameObject.Find("MidPlants"));
+        foreach (GameObject area in GameObject.FindGameObjectsWithTag("spawnArea")) {
+            CreatePlants(numOfPlants, area, placeHolderPlant, GameObject.Find("Plants"));
         }
     }
     // Returns a random xz position within the area occupied by a game object and spawns at a set height (raycast used elsewhere "sticks" spawned object to "area" object)
@@ -28,13 +24,12 @@ public class PlantLife : MonoBehaviour {
     public void StickObjectToBelow(GameObject obj, GameObject below) {
         RaycastHit hit;
         Vector3 dwn = obj.transform.TransformDirection(Vector3.down);
-        if(Physics.Raycast(obj.transform.position, dwn, out hit, 50)) obj.transform.position = hit.point;
+        if(Physics.Raycast(obj.transform.position, dwn, out hit, 500)) obj.transform.position = hit.point;
     }
     // Creates a set number of plants(arg1) in and area(arg2) of type(arg3) and makes them child of(arg4).
-    public void CreatePlants(int number, GameObject area, GameObject plantType, GameObject parent) {      
+    public void CreatePlants(int number, GameObject area, GameObject plantType, GameObject parent) {     
         for (var i = 0; i <= number; i++) {
-            //Instantiate plant object and assign it a name while making it a child of nearest plants.
-            GameObject thisPlant = Instantiate(plantType, FindPlantPosition(area), plantType.transform.rotation);
+            GameObject thisPlant = Instantiate(area.transform.position.y > 0 ? shallowPlant : deepPlant, FindPlantPosition(area), plantType.transform.rotation);
             StickObjectToBelow(thisPlant, floor);
             thisPlant.name = plantType.name;
             thisPlant.transform.parent = parent.transform;         

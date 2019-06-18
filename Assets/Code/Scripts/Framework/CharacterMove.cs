6cb95b5c83c5b26e7af438958040099fb33764c1
Nye <nyegoodall@gmail.com>
2019-06-18 15:51:47 +0100

    updated character controller

--- Assets/Code/Scripts/Framework/CharacterMove.cs
+++ Assets/Code/Scripts/Framework/CharacterMove.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
-    
+
     [SerializeField] private string horizontalInputName;
     [SerializeField] private string verticalInputName;
     [SerializeField] private float movementSpeed;
+
+    [SerializeField] private float slopeForce;
+    [SerializeField] private float slopeForceRayLength;
 
     private CharacterController charController;
 
@@ -25,21 +28,56 @@ public class CharacterMove : MonoBehaviour {
     }
 
     private void PlayerMovement() {
-    	float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
-    	float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+    	float vertInput = Input.GetAxis(verticalInputName);
+    	float horizInput = Input.GetAxis(horizontalInputName);
 
     	Vector3 forwardMovement = transform.forward * vertInput;
     	Vector3 rightMovement = transform.right * horizInput;
 
     	// Note: SimpleMove applies Time.deltaTime automatically so no need to do so above
-    	charController.SimpleMove(forwardMovement + rightMovement);
+    	charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+
+    	if((vertInput != 0 || horizInput != 0) && OnSlope()) {
+    		charController.Move(Vector3.down * charController.height / 2 * slopeForce * Time.deltaTime);
+    	}
+
+    	JumpInput();
     }
 
     private void JumpInput() {
-
+    	if(Input.GetKeyDown(jumpKey) && !isJumping) {
+    		isJumping = true;
+    		StartCoroutine(JumpEvent());
+    	}
     }
 
     private IEnumerator JumpEvent() {
-    	yield return null;
+    	charController.slopeLimit = 90.0f;
+    	float timeInAir = 0.0f;
+
+    	do {
+    		float jumpForce = jumpFallOff.Evaluate(timeInAir);
+    		charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+    		timeInAir += Time.deltaTime;
+    		yield return null;
+    	} while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+    	charController.slopeLimit = 45.0f;
+    	isJumping = false;
+    }
+
+    private bool OnSlope() {
+    	if (isJumping) {
+    		return false;
+    	}
+    	RaycastHit hit;
+
+    	if(Physics.Raycast(transform.position, Vector3.down, out hit, charController.height / 2 * slopeForceRayLength)) {
+    		if(hit.normal != Vector3.up) {
+    			return true;
+    		}
+    	}
+    	
+    	return false;
     }
 }

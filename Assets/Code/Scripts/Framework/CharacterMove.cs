7d002635801f56c34707e42ff8f740a7995c6db9
Nye <nyegoodall@gmail.com>
2019-06-21 04:49:42 +0100

    CharacterMove spaces to tabs

--- Assets/Code/Scripts/Framework/CharacterMove.cs
+++ Assets/Code/Scripts/Framework/CharacterMove.cs
@@ -4,119 +4,119 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 	[Header("Input Names")]
-    [SerializeField] private string horizontalInputName;
-    [SerializeField] private string verticalInputName;
-    [SerializeField] private string runInputName;
-    [SerializeField] private string crouchInputName;
-    [SerializeField] private string jumpInputName;
+	[SerializeField] private string horizontalInputName;
+	[SerializeField] private string verticalInputName;
+	[SerializeField] private string runInputName;
+	[SerializeField] private string crouchInputName;
+	[SerializeField] private string jumpInputName;
 
-    [Header("Movement")]
-    [SerializeField] private float walkSpeed;
-    [SerializeField] private float runSpeed;
-    [SerializeField] private float crouchSpeed;
-    [SerializeField] private float crouchCameraMove;
-    [SerializeField] private AnimationCurve jumpFallOff;
-    [SerializeField] private float jumpMultiplier;
-    private float movementSpeed;
- 	private bool isJumping;
+	[Header("Movement")]
+	[SerializeField] private float walkSpeed;
+	[SerializeField] private float runSpeed;
+	[SerializeField] private float crouchSpeed;
+	[SerializeField] private float crouchCameraMove;
+	[SerializeField] private AnimationCurve jumpFallOff;
+	[SerializeField] private float jumpMultiplier;
+	private float movementSpeed;
+	private bool isJumping;
 
-    [Header("GameObject")]
-    [SerializeField] private GameObject camera;
-    private CharacterController charController;
+	[Header("GameObject")]
+	[SerializeField] private GameObject camera;
+	private CharacterController charController;
 
-    [Header("Physics")] // Two varialbes, ray length is the length of the ray shooting down to detect floor, slopForce is the downwards force applied to remove jitters
-    [SerializeField] private float slopeForce;
-    [SerializeField] private float slopeForceRayLength;
+	[Header("Physics")] // Two varialbes, ray length is the length of the ray shooting down to detect floor, slopForce is the downwards force applied to remove jitters
+	[SerializeField] private float slopeForce;
+	[SerializeField] private float slopeForceRayLength;
 
-    // Called once after objects are initialized, used to initialize variables and get the Character Controller Component
-    private void Awake() {
-    	charController = GetComponent<CharacterController>();
-    	movementSpeed = walkSpeed;
-    }
+	// Called once after objects are initialized, used to initialize variables and get the Character Controller Component
+	private void Awake() {
+		charController = GetComponent<CharacterController>();
+		movementSpeed = walkSpeed;
+	}
 
-    // Called once per frame and calls the PlayerMovement function, which controls all player movement
-    private void Update() {
-    	PlayerMovement();
-    }
+	// Called once per frame and calls the PlayerMovement function, which controls all player movement
+	private void Update() {
+		PlayerMovement();
+	}
 
-    // Called once per frame, first 5 lines deal with the keyboard movement, then stops slope jittering, then calls other functions
-    private void PlayerMovement() {
-    	float vertInput = Input.GetAxis(verticalInputName);
-    	float horizInput = Input.GetAxis(horizontalInputName);
+	// Called once per frame, first 5 lines deal with the keyboard movement, then stops slope jittering, then calls other functions
+	private void PlayerMovement() {
+		float vertInput = Input.GetAxis(verticalInputName);
+		float horizInput = Input.GetAxis(horizontalInputName);
 
-    	Vector3 forwardMovement = transform.forward * vertInput;
-    	Vector3 rightMovement = transform.right * horizInput;
+		Vector3 forwardMovement = transform.forward * vertInput;
+		Vector3 rightMovement = transform.right * horizInput;
 
-    	// Note: SimpleMove applies Time.deltaTime automatically so no need to do so above
-    	charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+		// Note: SimpleMove applies Time.deltaTime automatically so no need to do so above
+		charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
 
-    	if((vertInput != 0 || horizInput != 0) && OnSlope()) {
-    		charController.Move(Vector3.down * charController.height / 2 * slopeForce * Time.deltaTime);
-    	}
+		if((vertInput != 0 || horizInput != 0) && OnSlope()) {
+			charController.Move(Vector3.down * charController.height / 2 * slopeForce * Time.deltaTime);
+		}
 
-    	Run();
-    	Crouch();
-    	JumpInput();
-    }
+		Run();
+		Crouch();
+		JumpInput();
+	}
 
-    // Checks to make sure player has pressed the jump key, and also is not already jumping
-    private void JumpInput() {
-    	if(Input.GetButtonDown(jumpInputName) && !isJumping) {
-    		isJumping = true;
-    		StartCoroutine(JumpEvent());
-    	}
-    }
+	// Checks to make sure player has pressed the jump key, and also is not already jumping
+	private void JumpInput() {
+		if(Input.GetButtonDown(jumpInputName) && !isJumping) {
+			isJumping = true;
+			StartCoroutine(JumpEvent());
+		}
+	}
 
-    // Called when player has jumped and isnt already jumping, and executes the jump
-    private IEnumerator JumpEvent() {
-    	charController.slopeLimit = 90.0f;
-    	float timeInAir = 0.0f;
+	// Called when player has jumped and isnt already jumping, and executes the jump
+	private IEnumerator JumpEvent() {
+		charController.slopeLimit = 90.0f;
+		float timeInAir = 0.0f;
 
-    	do {
-    		float jumpForce = jumpFallOff.Evaluate(timeInAir);
-    		charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
-    		timeInAir += Time.deltaTime;
-    		yield return null;
-    	} while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+		do {
+			float jumpForce = jumpFallOff.Evaluate(timeInAir);
+			charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+			timeInAir += Time.deltaTime;
+			yield return null;
+		} while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
 
-    	charController.slopeLimit = 45.0f;
-    	isJumping = false;
-    }
+		charController.slopeLimit = 45.0f;
+		isJumping = false;
+	}
 
-    // Raycast used to detect the angle of the floor below the player, and if the player is on a slope applies a downwards force to stop jitters while walking down
-    private bool OnSlope() {
-    	if (isJumping) {
-    		return false;
-    	}
-    	RaycastHit hit;
+	// Raycast used to detect the angle of the floor below the player, and if the player is on a slope applies a downwards force to stop jitters while walking down
+	private bool OnSlope() {
+		if (isJumping) {
+			return false;
+		}
+		RaycastHit hit;
 
-    	if(Physics.Raycast(transform.position, Vector3.down, out hit, charController.height / 2 * slopeForceRayLength)) {
-    		if(hit.normal != Vector3.up) {
-    			return true;
-    		}
-    	}
-    	
-    	return false;
-    }
+		if(Physics.Raycast(transform.position, Vector3.down, out hit, charController.height / 2 * slopeForceRayLength)) {
+			if(hit.normal != Vector3.up) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
 
-    // Called when the player presses the run key, and increases the players movement while the key is held down
-    private void Run() {
-    	if(Input.GetButtonDown(runInputName)) {
-    		movementSpeed = runSpeed;
-    	} else if(Input.GetButtonUp(runInputName)) {
-    		movementSpeed = walkSpeed;
-    	}
-    }
+	// Called when the player presses the run key, and increases the players movement while the key is held down
+	private void Run() {
+		if(Input.GetButtonDown(runInputName)) {
+			movementSpeed = runSpeed;
+		} else if(Input.GetButtonUp(runInputName)) {
+			movementSpeed = walkSpeed;
+		}
+	}
 
-    // Called when the player presses the crouch key, and lowers the camera while the key is held down
-    private void Crouch() {
-    	if(Input.GetButtonDown(crouchInputName)) {
-    		camera.transform.Translate(Vector3.down * crouchCameraMove);
-    		movementSpeed = crouchSpeed;
+	// Called when the player presses the crouch key, and lowers the camera while the key is held down
+	private void Crouch() {
+		if(Input.GetButtonDown(crouchInputName)) {
+			camera.transform.Translate(Vector3.down * crouchCameraMove);
+			movementSpeed = crouchSpeed;
 
-    	} else if(Input.GetButtonUp(crouchInputName)) {
-    		camera.transform.Translate(Vector3.up * crouchCameraMove);
-    		movementSpeed = walkSpeed;
-    	}
-    }
+		} else if(Input.GetButtonUp(crouchInputName)) {
+			camera.transform.Translate(Vector3.up * crouchCameraMove);
+			movementSpeed = walkSpeed;
+		}
+	}
 }

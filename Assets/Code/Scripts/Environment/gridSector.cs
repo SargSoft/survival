11786f8a49bbebe25eeccd5a36b3214f21be17f1
Jack-G-T <47399716+Jack-G-T@users.noreply.github.com>
2019-06-29 16:35:10 +0100

    Added functions to divide map area into zones

---
+++ Assets/Code/Scripts/Environment/gridSector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridSector : MonoBehaviour
+{
+	private Vector3 myPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPosition = transform.position;
+    }
+}

c7734fbba17d04678527fc14a22d03952a0f9fcf
Nye <nyegoodall@gmail.com>
2019-12-17 08:18:24 +0000

    Created Biome Settings asset file

---
+++ Assets/Code/Scripts/Environment/BiomeSettings.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BiomeSettings : ScriptableObject {
+	// Settings
+    [Header ("Intertidal Zone")]
+	public GameObject biome1ShallowFlora;
+    public GameObject biome1DeepFlora;
+    public GameObject biome1ShallowFauna;
+    public GameObject biome1DeepFauna;
+	
+    [Header ("Pelagic Zone")]
+	public GameObject biome2ShallowFlora;
+    public GameObject biome2DeepFlora;
+    public GameObject biome2ShallowFauna;
+    public GameObject biome2DeepFauna;
+
+    [Header ("Benthic Zone")]
+	public GameObject biome3ShallowFlora;
+    public GameObject biome3DeepFlora;
+    public GameObject biome3ShallowFauna;
+    public GameObject biome3DeepFauna;
+
+    [Header ("Abyssal Zone")]
+	public GameObject biome4ShallowFlora;
+    public GameObject biome4DeepFlora;
+    public GameObject biome4ShallowFauna;
+    public GameObject biome4DeepFauna;
+
+    [Header ("Reef")]
+	public GameObject biome5ShallowFlora;
+    public GameObject biome5DeepFlora;
+    public GameObject biome5ShallowFauna;
+    public GameObject biome5DeepFauna;
+
+    [Header ("Tropical")]
+	public GameObject biome6ShallowFlora;
+    public GameObject biome6DeepFlora;
+    public GameObject biome6ShallowFauna;
+    public GameObject biome6DeepFauna;
+}
